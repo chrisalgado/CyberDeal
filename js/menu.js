429f93138fc6a19ce9638ac548e4f9df29caa8c3
Christopher Alexander Salgado <cristopher.salgado1997@gmail.com>
2017-11-22 18:43:59 -0600

    modificación del index
    Ya está el menu y el banner

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var icono = document.getElementById("btnMenu");
+var menu = document.getElementById("navegacion");
+
+icono.addEventListener('click', function(){
+	menu.classList.toggle('mostrar');
+})
