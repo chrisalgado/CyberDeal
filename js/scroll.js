357c7f83366b9a14b629a68830abeb14646f3bfd
Christopher Alexander Salgado <cristopher.salgado1997@gmail.com>
2017-11-25 03:47:10 -0600

    .

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,13 @@
-$(function(){
-	
-	$('#inicio').click(function(){
-		$('body').animate({scrollTop:0},1000);
-	});
+function detectarScroll(){
+	var scrollvertical = $(window).scrollTop();
+}
+
+function muevete(){
+	$('#inicio').onclick('click', function(){
+		var body = $("html, body");
+ 		body.stop().animate({scrollTop:0}, 500, 'swing', function() { 
+		alert("Finished animating");
 });
+	});
+}
+
